524cb6fb73997bcf221d2369a542047a46fcc7da
guohuijuan1 <z4ghjdyx@foxmail.com>
2021-09-01 22:37:33 +0800

    Using webpack-dev-middleware

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,16 @@ module.exports = {
   },
   devtool: 'inline-source-map',
   devServer: {
+    // 资源托管路径
+    // webpack-dev-server serves bundled files from the directory defined in output.path, 
+    // i.e., files will be available under http://[devServer.host]:[devServer.port]/[output.publicPath]/[output.filename]
     static: './dist',
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    // http://localhost:3000/pub 可访问
+    publicPath: '/pub',
     // clean the /dist folder before each build
     clean: true,
   },
