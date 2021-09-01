2896f2ea82be52898395e6147fd6f087426d21c0
guohuijuan1 <z4ghjdyx@foxmail.com>
2021-09-01 23:06:35 +0800

    feat: SplitChunksPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,30 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
+  // entry: {
+  //   index: {
+  //     import: './src/index.js',
+  //     // The dependOn option allows to share the modules between the chunks:
+  //     dependOn: 'shared',
+  //   },
+  //   another: {
+  //     import: './src/another-module.js',
+  //     dependOn: 'shared',
+  //   },
+  //   shared: 'lodash',
+  // },
+  // optimization: {
+  //   // https://bundlers.tooling.report/code-splitting/multi-entry/
+  //   runtimeChunk: 'single',
+  // },
   entry: {
-    index: {
-      import: './src/index.js',
-      // The dependOn option allows to share the modules between the chunks:
-      dependOn: 'shared',
-    },
-    another: {
-      import: './src/another-module.js',
-      dependOn: 'shared',
-    },
-    shared: 'lodash',
+    index: './src/index.js',
+    another: './src/another-module.js',
   },
   optimization: {
-    // https://bundlers.tooling.report/code-splitting/multi-entry/
-    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+    },
   },
   output: {
     filename: '[name].bundle.js',
