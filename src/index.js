345c02b7b2e2a501c77354f1d8288f6bbba53f9e
guohuijuan1 <z4ghjdyx@foxmail.com>
2021-09-01 21:57:10 +0800

    feat: assert deal

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import _ from 'lodash';
+import './style.css';
+import ghj from './ghj2.jpg';
+import Data from './data.xml';
+import Notes from './data.csv';
+
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
     const element = document.createElement('div');
@@ -6,7 +23,16 @@ function component() {
     // Lodash, currently included via a script, is required for this line to work
     // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    element.classList.add('hello');
+
+    const myIcon = new Image();
+    myIcon.src = ghj;
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
+
+    console.log(Notes);
     return element;
   }
   
